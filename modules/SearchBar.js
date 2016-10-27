06ddb0ec213b4ec78d56364932293f63c6dd030a
nik <nik_rb@yahoo.com>
2016-10-27 18:23:52 +0100

    hard code search input text and button as jquery find failed

--- modules/SearchBar.js
+++ modules/SearchBar.js
@@ -2,30 +2,32 @@
 class SearchBar {
   constructor( getData, searchBoxDiv, dropListDiv, listItemSelected, searchButtonClicked){
     this.searchTextChange = this.searchTextChange.bind(this);
-    this.searchButtonClick = this.searchButtonClick.bind(this);
+    this.searchClickHandler = this.searchClickHandler.bind(this);
     this.itemSelected = this.itemSelected.bind(this);
     this.updateListDropdown = this.updateListDropdown.bind(this);
     this.getData = getData;
     this.searchBoxDiv = $('#'+searchBoxDiv);
     this.dropListDiv = $('#'+dropListDiv);
-    const inputBox = this.searchBoxDiv.find( 'input');
-    inputBox.keyup( this.searchTextChange);
+    const inputBox = $( '#search_text');
+    // const inputBox = this.searchBoxDiv.find( 'input');
+    inputBox.on( "keyup", this.searchTextChange);
 
-    const searchButton = this.searchBoxDiv.find( 'button');
-    searchButton.on( 'click', this.searchButtonClick);
+    // const searchButton = this.searchBoxDiv.find( 'button');
+    const searchButton = $('#search_button');
+    searchButton.on( 'click', this.searchClickHandler);
 
     this.selectList = this.dropListDiv.find( 'select');
     this.selectList.on( 'change', this.itemSelected);
-    
+
     this.callbackItemSelected = listItemSelected;
     this.callbackSearchButton = searchButtonClicked;
     this.current_search_text = "";
   }
 
   updateListDropdown( data){
-    this.selectList.empty(); // find( 'option').remove();
+    this.selectList.empty();
     if( data.length){
-  		this.dropListDiv.show( 100);
+  		this.dropListDiv.removeClass( 'hide').show( 100);
       for( var i=0; i<data.length; i++){
         var item = data[i];
         this.selectList.append( "<option value='"+item.id+"' >"+
@@ -48,9 +50,9 @@ class SearchBar {
 		this.dropListDiv.hide( 100);
 		this.callbackItemSelected( e.target.value);
   }
-  searchButtonClick( e){
-		this.dropListDiv.hide( 100);
+  searchClickHandler( e){
+    console.log( "searchClickHandler");
+		this.dropListDiv.addClass( 'hide').hide( 100);
 		this.callbackSearchButton( this.current_search_text);
   }
 }
-
